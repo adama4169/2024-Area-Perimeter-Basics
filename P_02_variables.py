20a73022bcae154a0f92c0e0290befc669d6a4fc
adama4169 <adama4169@masseyhigh.school.nz>
2025-07-17 12:50:48 +1200

    Add files via upload
    test2

---
+++ P_02_variables.py
@@ -0,0 +1,11 @@
+# Code to learn about variables
+
+# Variables can be a string (note the speech marks)
+greeting = "helloo"
+
+# Variables can be a number
+fav_num =  6
+num_string = "5"
+
+# Constants are uppercase
+PI = 3.14
